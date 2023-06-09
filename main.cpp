1b21c651df9a1e43da1f983d394b2be57fddb5b6
aaaaa ttttt <ryudai.0728@gmail.com>
2023-06-09 12:49:11 +0900

    00_03_確認課題

--- main.cpp
+++ main.cpp
@@ -14,12 +14,22 @@ typedef struct Vector3 {
 //4x4行列表示
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
-void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
 	for (int row = 0; row < 4; ++row) {
 		for (int column = 0; column < 4; ++column) {
 			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
 		}
 	}
+
+	Novice::ScreenPrintf(x, y - kRowHeight, "%s", label);
+}
+
+// ベクトル表示
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
 }
 
 
@@ -90,7 +100,7 @@ Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
 }
 
 
-const char kWindowTitle[] = "LE2D_18_ニヘイリュウダイ_MT3_02";
+const char kWindowTitle[] = "LE2D_18_ニヘイリュウダイ_MT3_00_03";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -141,8 +151,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		
-
-
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, kRowHeight * 2, translateMatrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 7, scaleMatrix, "scaleMatrix");
 
 
 
