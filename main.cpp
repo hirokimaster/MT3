d8941ba973c0eef576d29723914c474e198df2de
aaaaa ttttt <ryudai.0728@gmail.com>
2023-06-09 12:17:35 +0900

    00_02_確認課題

--- main.cpp
+++ main.cpp
@@ -178,11 +178,25 @@ Matrix4x4 Transpose(const Matrix4x4& m) {
 }
 
 //6. 単位行列の作成
-// 単位行列
 Matrix4x4 MakeIdentityMatrix() {
-	static const Matrix4x4 result{ 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-								  0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+	Matrix4x4 result;
 
+	result.m[0][0] = 1.0f;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = 1.0f;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = 1.0f;
+	result.m[2][3] = 0.0f;
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
 	return result;
 }
 
