5d29639be5e67c8f3f493e7c3dcc2449071d655f
aaaaa ttttt <ryudai.0728@gmail.com>
2023-06-09 16:03:31 +0900

    初期設定

--- main.cpp
+++ main.cpp
@@ -22,6 +22,13 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
 	}
 }
 
+// 三次元ベクトル表示
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
 
 const char kWindowTitle[] = "LE2D_18_ニヘイリュウダイ_MT3_02";
 
